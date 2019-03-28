2dad7a81098ebe1b7bfb7cb075a4946ae84a546d
Sean Nemann <nemann.3@buckeyemail.osu.edu>
2019-03-28 19:55:15 -0400

    Moved method to setup Spotify playback from html page to javascript file. This was initially in html file since an issue occurred in Firefox where setup would run before the SpotifyPlayer javascript file was imported, but this issue has resolved itself

--- MusicTry3/wwwroot/playlist.js
+++ MusicTry3/wwwroot/playlist.js
@@ -11,6 +11,10 @@ $(document).ready(function () {
     var username = window.sessionStorage.getItem("username");
     var userstatus = window.sessionStorage.getItem("userstatus");
 
+    if (authenticationToken != null) {
+        setupSpotifyPlayback(authenticationToken, sessionId, playlistId);
+    }
+
     if (userstatus !== "master") {
         $("#play").hide();
         $("#progressBarDiv").hide();
@@ -371,4 +375,41 @@ function exitSession() {
     window.sessionStorage.removeItem("keep-alive");
     window.sessionStorage.removeItem("authentication-token");
     window.location.assign("/home");
+}
+
+
+function setupSpotifyPlayback(authToken) {
+    window.onSpotifyWebPlaybackSDKReady = () => {
+        const token = authToken;
+        const player = new Spotify.Player({
+            name: 'Web Playback SDK Quick Start Player',
+            getOAuthToken: cb => { cb(token); }
+        });
+
+        // Error handling
+        player.addListener('initialization_error', ({ message }) => { console.error(message); });
+        player.addListener('authentication_error', ({ message }) => { console.error(message); });
+        player.addListener('account_error', ({ message }) => { console.error(message); });
+        player.addListener('playback_error', ({ message }) => { console.error(message); });
+
+        // Playback status updates
+        player.addListener('player_state_changed', state => {
+            console.log(state);
+            UpdatePlayback(state);
+        });
+
+        // Ready
+        player.addListener('ready', ({ device_id }) => {
+            deviceID = device_id;
+            console.log('Ready with Device ID', device_id);
+        });
+
+        // Not Ready
+        player.addListener('not_ready', ({ device_id }) => {
+            console.log('Device ID has gone offline', device_id);
+        });
+
+        // Connect to the player!
+        player.connect();
+    };
 }
